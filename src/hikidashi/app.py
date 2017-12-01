cd1adf4c664c2ed9ffc2f236b79a41d58774f21c
Kimihiro N <kimihiro.nisimura@gmail.com>
2017-12-01 09:53:01 +0900

    Enabled swagger ui for debug

--- src/hikidashi/app.py
+++ src/hikidashi/app.py
@@ -1,9 +1,22 @@
 # coding: utf-8
 
 from flask import Flask, jsonify
+from flask import Response
+import requests
+
+from hikidashi.settings import SWAGGER_UI_HOST
 
 
 app = Flask(__name__)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    if SWAGGER_UI_HOST:
+        res = requests.get(f'{SWAGGER_UI_HOST}/{path}')
+        return Response(res.content, content_type=res.headers['content-type'])
+    return jsonify({})
 
 
 @app.route('/health')
---
+++ src/hikidashi/settings.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from os import environ
+
+
+SWAGGER_UI_HOST = environ.get('SWAGGER_UI_HOST')
